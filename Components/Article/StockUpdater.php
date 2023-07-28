0c6f8fa9ac3bf133c9fc1741595f0fec2ae95227
Alexander Wolf <alexander.wolf@8mylez.com>
2023-07-28 11:03:24 +0200

    Add logging for update of stocks

--- Components/Article/StockUpdater.php
+++ Components/Article/StockUpdater.php
@@ -10,6 +10,7 @@ use SgateShipFromStore\Components\Article\Encapsulation\InventoryContainer;
 use SgateShipFromStore\Framework\Sequence\InlineRecordHandling;
 use Shopware\Components\Model\ModelManager;
 use Shopware\Models\Article\Detail as ArticleDetailEntity;
+use Psr\Log\LoggerInterface;
 
 class StockUpdater extends InlineRecordHandling
 {
@@ -28,18 +29,26 @@ class StockUpdater extends InlineRecordHandling
      */
     private $articleRepository;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         ModelManager $modelManager,
-        ArrayEncapsulation $config
+        ArrayEncapsulation $config,
+        LoggerInterface $logger
     ) {
         $this->modelManager = $modelManager;
         $this->config = $config;
         $this->articleRepository = $modelManager->getRepository(ArticleDetailEntity::class);
+        $this->logger = $logger;
     }
 
     public function updateStocks(InventoryContainer $inventories, int $shopId): void
     {
         $productCodeField = $this->config->get($shopId)->get('productCode') === 'ean' ? 'ean' : 'number';
+        $logOutput = [];
 
         /** @var Inventory $inventory */
         foreach ($inventories as $inventory) {
@@ -50,9 +59,11 @@ class StockUpdater extends InlineRecordHandling
             }
 
             $article->setInStock($inventory->get('visible'));
+            $logOutput[$article->getNumber()] = $inventory->get('visible');
         }
 
         $this->modelManager->flush();
+        $this->logger->info('Stocks successfully updated', $logOutput);
     }
 
     protected function buildContainer(Container $container): Container
