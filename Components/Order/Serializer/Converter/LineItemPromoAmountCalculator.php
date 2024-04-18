e5b1f1c8a409f0daac6f4069953d02977358798c
Timo Ebel <timo.ebel@shopgate.com>
2024-04-18 09:37:27 +0200

    CURB-3928 Fix unitPromoAmount calculation.

--- Components/Order/Serializer/Converter/LineItemPromoAmountCalculator.php
+++ Components/Order/Serializer/Converter/LineItemPromoAmountCalculator.php
@@ -4,6 +4,7 @@ namespace SgateShipFromStore\Components\Order\Serializer\Converter;
 
 use Dustin\Encapsulation\EncapsulationInterface;
 use Dustin\ImpEx\Serializer\Converter\UnidirectionalConverter;
+use SgateShipFromStore\Components\Util\LineItem;
 
 class LineItemPromoAmountCalculator extends UnidirectionalConverter
 {
@@ -28,7 +29,11 @@ class LineItemPromoAmountCalculator extends UnidirectionalConverter
 
     public function convert($value, EncapsulationInterface $object, string $path, string $attributeName, ?array $normalizedData = null)
     {
-        if ($normalizedData === null) {
+        if (
+            $normalizedData === null ||
+            empty($normalizedData['product']) ||
+            !LineItem::isProductLineItem($normalizedData)
+        ) {
             return $value;
         }
 
--- Components/Order/Serializer/Converter/LineItemUnitPromoAmountCalculator.php
+++ Components/Order/Serializer/Converter/LineItemUnitPromoAmountCalculator.php
@@ -4,15 +4,22 @@ namespace SgateShipFromStore\Components\Order\Serializer\Converter;
 
 use Dustin\Encapsulation\EncapsulationInterface;
 use Dustin\ImpEx\Serializer\Converter\UnidirectionalConverter;
+use SgateShipFromStore\Components\Util\LineItem;
 
 class LineItemUnitPromoAmountCalculator extends UnidirectionalConverter
 {
     public function convert($value, EncapsulationInterface $object, string $path, string $attributeName, ?array $normalizedData = null)
     {
-        if ($normalizedData === null) {
+        if (
+            $normalizedData === null ||
+            empty($normalizedData['product']) ||
+            !LineItem::isProductLineItem($normalizedData)
+        ) {
             return $value;
         }
 
-        return round($normalizedData['extendedPrice'] - $normalizedData['price'], 2);
+        $priceBase = $normalizedData['product']['salePrice'] ?? $normalizedData['product']['price'];
+
+        return round($normalizedData['extendedPrice'] - $priceBase, 2);
     }
 }
